5237b9381027819383510de85ad22e7aaa6b40eb
Céline Pervès <cperves@unistra.fr>
2026-05-11 15:22:24 +0200

    js corrections

--- amd/src/changestatus.js
+++ amd/src/changestatus.js
@@ -2,13 +2,13 @@ import Ajax from 'core/ajax';
 import notification from "core/notification";
 
 /**
- * asyncchangestatus
+ * Change status of task
  * @param {int} id
  * @returns {Promise<void>}
  */
-export const asyncchangestatus = async (id) => {
-    //AJAX call to webservice
-    var selectelt = document.getElementById('menustatus_select_'+id);
+export const asyncchangestatus = async(id) => {
+    // AJAX call to webservice.
+    var selectelt = document.getElementById('menustatus_select_' + id);
     await new Promise(resolve => {
         return Ajax.call([{
             methodname: 'tool_import_user_datas_change_task_status',
